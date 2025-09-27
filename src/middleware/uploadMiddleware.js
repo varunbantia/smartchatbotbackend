7441fc42b47d65380e40daa980a9f4947a707819
varunbantia <varunjainak123@gmail.com>
2025-09-27 13:50:04 +0530

    backend

--- src/middleware/uploadMiddleware.js
+++
@@ -1,5 +0,0 @@
-// src/middleware/uploadMiddleware.js
-import multer from "multer";
-
-// Configure multer to store files temporarily in the 'uploads/' directory
-export const upload = multer({ dest: "uploads/" });
